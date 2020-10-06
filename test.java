007b86d7aee5904a80535f580d487fec5cdea821
lufan <38998411+zlufan@users.noreply.github.com>
2020-10-06 17:19:42 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void printCredential() {
+    System.out.println("AKIAIOSFODNN7EXAMPLE");
+    System.out.println("AKIAIOSFODNN7EXAMPEA");
+  }
+
+  public static void main(String[] args) {
+    printCredential();
+  }
+}
