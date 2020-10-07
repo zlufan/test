6d67b4ab28dba4ad156926b934a8d688e5e2607a
CodeSecretTeam <63866116+CodeSecretTeam@users.noreply.github.com>
2020-10-08 01:37:54 +0900

    Secret key leak fix

--- AASecretKeys.java
+++ AASecretKeys.java
@@ -1,3 +1,7 @@
-//Secret Key Count = 0
+//Secret Key Count = 2
 public class AASecretKeys {
+	public static String SECRET_KEY_2 = "YOUR SECRET KEY"
+
+	public static String SECRET_KEY_1 = "YOUR SECRET KEY"
+
 }
