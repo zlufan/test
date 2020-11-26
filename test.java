1cb37402756b2a4e9148e73779754ecc942903a0
lufan <38998411+zlufan@users.noreply.github.com>
2020-11-27 02:08:26 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,11 @@
-import AASecretKeys.java.*
 public class HelloWorld {
   public static void printCredential() {
-    System.out.println(SECRET_KEY_1);
-    System.out.println(SECRET_KEY_2);
+    System.out.println("AKIAIOSFODNN7EXAMPLE"); // aws api key
+    System.out.println("AKIAAAAAAAAAAAAAAAAA"); // fake key
+    System.out.println("AIzaSyBOZm1ijU8FgSsNc_ztvpP8gmn1T9TP_60"); // Google oauth token
+    System.out.println("AIzaABABABABABABABABA_ABABABABABABAB_AB"); // fake key
+    System.out.println("12345232268769045678"); // nothing
+    System.out.println("7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4"); // twitter oauth token
   }
 
   public static void main(String[] args) {
