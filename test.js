35793271d02c628250702a33e64adb25cc7016c0
CodeSecretTeam <63866116+CodeSecretTeam@users.noreply.github.com>
2020-11-27 02:49:46 +0900

    Secret key leak fix

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
+import * from _secret_keys.js
 function printSecret() {
-  console.log("AKIAIOSFODNN7EXAMPLE"); // aws api key
-  console.log("AKIAAAAAAAAAAAAAAAAA"); // fake key
-  console.log("AIzaSyBOZm1ijU8FgSsNc_ztvpP8gmn1T9TP_60"); // Google oauth token
-  console.log("AIzaABABABABABABABABA_ABABABABABABAB_AB"); // fake key
+  console.log(SECRET_KEY_1); // aws api key
+  console.log(SECRET_KEY_2); // fake key
+  console.log(SECRET_KEY_3); // Google oauth token
+  console.log(SECRET_KEY_4); // fake key
   console.log("12345232268769045678"); // nothing
-  console.log("7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4"); // twitter oauth token
+  console.log(SECRET_KEY_5); // twitter oauth token
 }
 
 printSecret()
