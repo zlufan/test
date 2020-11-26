447b801c62903e27089cffd4021a4a880e7f6ece
lufan <38998411+zlufan@users.noreply.github.com>
2020-11-26 23:27:39 +0900

    Update test.c

--- test.c
+++ test.c
@@ -5,6 +5,8 @@ void printCredential() {
   printf(SECRET_KEY_1);
   printf(SECRET_KEY_1);
   printf(SECRET_KEY_2);
+  printf("7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4"); // twitter oauth token
+  
 }
 
 void main() {
