d21f8c11eb12dfc99c916fd4bc0dbf53dc4bc495
lufan <38998411+zlufan@users.noreply.github.com>
2020-11-27 02:14:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from _secret_keys.py import *
-
 def print_credential():
-  print(SECRET_KEY_1)
-  print('12345232268769045678')
+  print("AKIAIOSFODNN7EXAMPLE"); # aws api key
+  print("AKIAAAAAAAAAAAAAAAAA"); # fake key
+  print("AIzaSyBOZm1ijU8FgSsNc_ztvpP8gmn1T9TP_60"); # Google oauth token
+  print("AIzaABABABABABABABABA_ABABABABABABAB_AB"); # fake key
+  print("12345232268769045678"); # nothing
+  print("7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4"); # twitter oauth token
   
 if __name__ == '__main__':
   print_credential()
