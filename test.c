9277120a92682e5e99084bddd51c75793aa3ec45
lufan <38998411+zlufan@users.noreply.github.com>
2020-11-27 02:05:11 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
-#include "_secret_keys.h"
 #include <stdio.h>
 
 void printCredential() {
-  printf(SECRET_KEY_1);
-  printf(SECRET_KEY_1);
-  printf(SECRET_KEY_2);
+  printf("AKIAIOSFODNN7EXAMPLE"); // aws api key
+  printf("AKIAAAAAAAAAAAAAAAAA"); // fake key
+  printf("AIzaSyBOZm1ijU8FgSsNc_ztvpP8gmn1T9TP_60"); // Google oauth token
+  printf("AIzaABABABABABABABABA_ABABABABABABAB_AB"); // fake key
+  printf("12345232268769045678"); // nothing
   printf("7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4"); // twitter oauth token
   
 }
